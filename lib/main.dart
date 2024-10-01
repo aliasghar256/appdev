5107e18ddfdb031ac32d0715cfa1edf2de6daa7f
Ali Asghar Hussain <alih11737@gmail.com>
2024-10-01 11:02:52 +0500

    data rendering in-class

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,84 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
+}
+
+class PostListUser extends StatefulWidget {
+  const PostListUser({super.key});
+
+  @override
+  State<PostListUser> createState() => _PostListUserState();
+}
+
+class _PostListUserState extends State<PostListUser> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: FutureBuilder(
+            future: fetchAllPosts(),
+            builder: (context, snap) {
+              if (snap.hasData)
+                return ListView.builder(itemBuilder: (c, i) {
+                  var _item = snap.data?[i];
+                  return ListTile(
+                    title: Text(_item!.title),
+                    subtitle: Text(_item!.content),
+                    leading: CircleAvatar(
+                      child: Image.network(_item.image),
+                    ),
+                  );
+                });
+              else if (snap.hasError) {
+                return Text("Error");
+              }
+              return Center(child: CircularProgressIndicator());
+            }),
+      ),
+    );
+  }
+
+  Future<List<Post>> fetchAllPosts() async {
+    final response =
+        await http.get(Uri.parse('https://jsonplaceholder.org/posts'));
+    if (response.statusCode == 200) {
+      List jsonResponse = jsonDecode(response.body);
+      return jsonResponse.map((post) => Post.fromJson(post)).toList();
+    } else {
+      throw Exception('Failed to load posts');
+    }
+  }
+}
+
+class Post {
+  final int id;
+  final String slug;
+  final String title;
+  final String content;
+  final String image;
+
+  Post({
+    required this.id,
+    required this.slug,
+    required this.title,
+    required this.content,
+    required this.image,
+  });
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      id: json['id'],
+      slug: json['slug'],
+      title: json['title'],
+      content: json['content'],
+      image: json['image'],
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -31,7 +108,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const PostListUser(),
     );
   }
 }
