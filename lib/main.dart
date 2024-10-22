4b74c81eabf33860d02fb3efb33d5a22e79384ae
Ali Asghar Hussain <alih11737@gmail.com>
2024-10-22 10:13:41 +0500

    Almost there

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(
@@ -14,10 +16,19 @@ void main() {
 }
 
 class Counter with ChangeNotifier {
-  int _count = 0;
-  int get count => _count;
-  void increment() {
-    _count++;
+  List<Post> _posts = [];
+  List<Post> get posts => _posts;
+  int get postLength => _posts.length;
+
+  void fetchPost() async {
+    final response =
+        await http.get(Uri.parse('https://jsonplaceholder.org/posts'));
+    if (response.statusCode == 200) {
+      List jsonResponse = jsonDecode(response.body);
+      _posts = jsonResponse.map((post) => Post.fromJson(post)).toList();
+    } else {
+      throw Exception('Failed to load posts');
+    }
     notifyListeners();
   }
 }
@@ -61,6 +72,32 @@ class MyHomePage extends StatelessWidget {
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
+    );
+  }
+}
+
+class Post {
+  final int id;
+  final String slug;
+  final String title;
+  final String content;
+  final String image;
+
+  Post({
+    required this.id,
+    required this.slug,
+    required this.title,
+    required this.content,
+    required this.image,
+  });
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      id: json['id'],
+      slug: json['slug'],
+      title: json['title'],
+      content: json['content'],
+      image: json['image'],
     );
   }
 }
